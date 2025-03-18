255993b4f80d5a0e5c1d21b3e97eb4a2162bbaa1
Benefrancis do Nascimento <benefrancis@gmail.com>
2025-03-18 03:49:22 -0300

    Revert "feat: dados ibge e funai"
    This reverts commit 07c25bcf

--- src/main/java/br/com/benefrancis/mapas/dto/EstadoDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.benefrancis.mapas.dto;
-
-import lombok.Builder;
-
-@Builder
-public record EstadoDTO(Long id, String nome, PaisDTO pais) {
-}
--- src/main/java/br/com/benefrancis/mapas/dto/MunicipioDTO.java
+++
@@ -1,6 +0,0 @@
-package br.com.benefrancis.mapas.dto;
-
-import lombok.Builder;
-
-@Builder
-public record MunicipioDTO(Long id, String nome, EstadoDTO estado) {}
